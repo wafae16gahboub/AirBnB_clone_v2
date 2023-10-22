69be9359ba616a598f9a16c50af9cf5935d2c27f
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-10-22 07:18:59 -0700

    task

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,13 +6,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
@@ -20,6 +23,6 @@ def c_route(text):
     text = text.replace('_', ' ')
     return "C " + text
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
