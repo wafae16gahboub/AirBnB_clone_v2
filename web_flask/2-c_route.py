381c9fdb42e51d43edbd35385a680c9ba36f0a44
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-10-22 07:18:02 -0700

    task

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A Flask web application with routes.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    # Replace underscores (_) with spaces in the text variable
+    text = text.replace('_', ' ')
+    return "C " + text
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
